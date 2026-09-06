fadef1de207cb6dde51ac73e84af259912a24236
Aditya Wadewale <wadewaleaditya@gmail.com>
2026-09-06 22:11:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -208,17 +208,17 @@ html, body, [class*="css"] {
     }
 
     .hero {
-        padding: 22px;
-        border-radius: 20px;
+        padding: 15px;
+        border-radius: 15px;
         
     }
 
     .hero h1 {
-        font-size: 29px;
+        font-size: 22px;
     }
 
     .hero p {
-        font-size: 14px;
+        font-size: 12px;
     }
 
     .metric-card {
